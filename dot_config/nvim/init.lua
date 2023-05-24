9ebe2b78937f61e6044b2d95dbd48840d0bb3bbf
Lukas Kunt <kuntl@merck.com>
2023-05-24 23:09:31 +0200

    added auto apply to work with chezmoi vim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,43 +3,23 @@
 =====================================================================
 ==================== READ THIS BEFORE CONTINUING ====================
 =====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, and understand
-  what your configuration is doing.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
 --]]
--- Set <space> as the leader key
--- See `:help mapleader`
---  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+vim.api.nvim_create_augroup("buffer", {clear= true})
+vim.api.nvim_create_autocmd({"BufNewFile", "BufReadPre", "BufRead"}, {
+   callback = function()
+     print(vim.fn.expand("%"))
+     vim.g.mychezmoi = 1
+   end,
+   pattern=vim.fn.expand("~") .. "/.local/share/chezmoi/*",
+   group = "buffer",
+   desc = "mydebug",
+})
+
+
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
@@ -511,5 +491,20 @@ cmp.setup {
   },
 }
 
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
+vim.api.nvim_create_autocmd("BufWritePost", {
+  --[[
+   callback = function()
+     if vim.g.mychezmoi == 1 then
+         print("Hellooooo")
+         vim.fn.jobstart('chezmoi apply')
+     end
+   end,
+   --]]
+   callback = function()
+      print("Helloooo")
+      vim.fn.jobstart('nohup chezmoi apply &')
+--      os.execute('chezmoi apply')
+   end,
+   group = "buffer",
+   pattern = "*/chezmoi-edit*",
+})
