84aa3f596345bb563fee474de5dead8bce8a5ebb
Ubuntu <ubuntu@ip-172-31-29-3.us-east-2.compute.internal>
2023-10-20 02:01:59 +0000

    added python server handlers

--- flask_api.py
+++ flask_api.py
@@ -9,36 +9,14 @@ from threading import Thread
 from dotenv import dotenv_values
 import openai
 import re
+import sqlite3
+import json
+from handlers import check_prompt_safety, check_response_safety, DBHandler
+from handlers import data_to_history
+
 
 openai.api_key = dotenv_values()['OPENAI_API_KEY']
 
-def check_prompt_safety(prompt):
-    pattern = r'\b(?i)(yes\.|yes)\b'
-    prompt_check = "QESTION={\n" + prompt + "/n}/nIs the QUESTION offensive, harmful or dangerous? Answer YES or NO only!"
-    try:
-        chat = openai.ChatCompletion.create( 
-            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_check}]
-        ) 
-        response = chat['choices'][0]['message']['content']
-        if len(re.findall(pattern, response)) > 0:
-            return True
-        return False
-    except Exception as e:
-        return False
-
-def check_response_safety(response):
-    pattern = r'\b(?i)(yes\.|yes)\b'
-    prompt_check = "RESPONSE={\n" + response + "/n}/nIs the RESPONSE offensive, harmful, dangerous or does it contain bias? Answer YES or NO only!"
-    try:
-        chat = openai.ChatCompletion.create( 
-            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_check}]
-        ) 
-        response = chat['choices'][0]['message']['content']
-        if len(re.findall(pattern, response)) > 0:
-            return True
-        return False
-    except Exception as e:
-        return False
 
 
 parser = argparse.ArgumentParser()
@@ -76,6 +54,7 @@ else:
 
 app = Flask(__name__)
 CORS(app)
+# db = DBHandler()
 
 @app.route("/get_session_history", methods= ['POST'])
 def get_session_history():
@@ -88,20 +67,24 @@ def get_session_history():
 
 @app.route("/get_sessions")
 def get_sessions():
-    return [
-        {'id': 1, 'name': 'foo1'},
-        {'id': 2, 'name': 'foo2'},
-            ]
+    db = DBHandler()
+    return {"sessions": [{'id': id, 'name': name} for id, name in zip(*db.names_and_ids)]}
 
 INPUT_SAFETY = False
 
-history = ""
-
 @app.route("/generate", methods = ['POST'])
 def generate():
-    global history
     data = request.get_json(force=True)
-    print(f"Input data: {data}")
+    db = DBHandler()
+    print(f"Database ids: {db.ids}")
+    if db.contains_id(data['id']):
+        conversation = db.fetch_id(data['id'])
+        history = data_to_history(conversation['data'])
+    else:
+        history = ""
+    
+    print(f"Current history is {history}")
+
     prompt = f"""### Instruction:
     Use the following Input and come up with a structured response in the style of Ned Flanders given the context. Be concise!
     ### Context:
@@ -117,13 +100,10 @@ def generate():
     
     # Tokenize the input
     input_ids = tokenizer(
-        # data['prompt'],
         prompt,
         return_tensors="pt", 
         truncation=True).input_ids.cuda()
     
-    # Support for streaming of tokens within generate requires 
-    # generation to run in a separate thread
     generation_kwargs = dict(
         input_ids = input_ids,
         max_new_tokens=data['parameters'].get('max_new_tokens', 100), 
@@ -137,41 +117,9 @@ def generate():
     
     # return {"generated_text" : tokenizer.batch_decode(outputs, skip_special_tokens=True)[0][len(data['prompt']):]}
     output_text = tokenizer.batch_decode(outputs.detach().cpu().numpy(), skip_special_tokens=True)[0][len(prompt):]
-    history += f"QESTION={data['prompt']}\nANSWER={output_text}"
+    db.update_by_id(data['id'], {'chatPrompt': data['prompt'], 'botMessage': output_text})
+    print("database ids: ", db.ids)
+    # history += f"QESTION={data['prompt']}\nANSWER={output_text}"
     return {"generated_text" : output_text}
-
-# @app.route("/generate/stream", methods = ['POST'])
-# def generate_stream():
-#     data = request.get_json(force=True)
-    
-#     # Tokenize the input
-#     input_ids = tokenizer(
-#         data['prompt'],
-#         return_tensors="pt", 
-#         truncation=True).input_ids.cuda()
-    
-#     # Support for streaming of tokens within generate requires 
-#     # generation to run in a separate thread
-#     streamer = TextIteratorStreamer(tokenizer, skip_prompt = True)
-#     generation_kwargs = dict(
-#         input_ids = input_ids,
-#         streamer = streamer, 
-#         max_new_tokens=data['parameters'].get('max_new_tokens', 100), 
-#         do_sample=data['parameters'].get('do_sample', True),
-#         top_p=data['parameters'].get('top_p', 0.9),
-#         temperature=data['parameters'].get('temperature', 0.7),
-#         use_cache=True
-#     )
-
-#     thread = Thread(target = model.generate, kwargs = generation_kwargs)
-#     thread.start()
-
-    # def f():
-    #     completion = ""
-    #     for r in streamer:
-    #         yield r[len(completion):]
-    #         completion = r 
-    
-    # return Response(stream_with_context(f()), mimetype = "text/event-stream")
 
 app.run(host="0.0.0.0", port = 7861)
---
+++ handlers.py
@@ -0,0 +1,93 @@
+from dotenv import dotenv_values
+import openai
+import re
+import sqlite3
+import json
+
+def check_prompt_safety(prompt):
+    openai.api_key = dotenv_values()['OPENAI_API_KEY']
+    pattern = r'\b(?i)(yes\.|yes)\b'
+    prompt_check = "QESTION={\n" + prompt + "/n}/nIs the QUESTION offensive, harmful or dangerous? Answer YES or NO only!"
+    try:
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_check}]
+        ) 
+        response = chat['choices'][0]['message']['content']
+        if len(re.findall(pattern, response)) > 0:
+            return True
+        return False
+    except Exception as e:
+        return False
+
+def check_response_safety(response):
+    openai.api_key = dotenv_values()['OPENAI_API_KEY']
+    pattern = r'\b(?i)(yes\.|yes)\b'
+    prompt_check = "RESPONSE={\n" + response + "/n}/nIs the RESPONSE offensive, harmful, dangerous or does it contain bias? Answer YES or NO only!"
+    try:
+        chat = openai.ChatCompletion.create( 
+            model="gpt-3.5-turbo", messages=[{"role": "user", "content": prompt_check}]
+        ) 
+        response = chat['choices'][0]['message']['content']
+        if len(re.findall(pattern, response)) > 0:
+            return True
+        return False
+    except Exception as e:
+        return False
+
+def data_to_history(data):
+    history = ""
+    for inp_resp in data:
+        history += f"QESTION={inp_resp['chatPrompt']}\nANSWER={inp_resp['botMessage']}\n"
+    return history
+
+
+
+class DBHandler:
+    def __init__(self) -> None:
+        self.conn = sqlite3.connect('my_test_database.db')
+        self.cursor = self.conn.cursor()
+        self.table = "history"
+        self.cursor.execute(f'''
+            CREATE TABLE IF NOT EXISTS {self.table} (
+                id STRING PRIMARY KEY,
+                data TEXT
+            )''')
+    
+    @property
+    def ids(self):
+        self.cursor.execute(f'SELECT id FROM {self.table}')
+        return [row[0] for row in self.cursor.fetchall()]
+
+    @property
+    def names_and_ids(self):
+        self.cursor.execute(f'SELECT id FROM {self.table}')
+        ids = [row[0] for row in self.cursor.fetchall()]
+        self.cursor.execute(f'SELECT data FROM {self.table}')
+        all_data = [row[0] for row in self.cursor.fetchall()]
+        names = [json.loads(data)['data'][0].get('chatPrompt', 'No name') for data in all_data]
+        return names, ids
+    
+    def contains_id(self, id):
+        return id in self.ids
+    
+    def fetch_id(self, id):
+        self.cursor.execute(f'SELECT data FROM {self.table} WHERE id = ?', (id,))
+        data_str = self.cursor.fetchone()
+        if data_str:
+            return json.loads(data_str[0])
+        else:
+            return 
+    
+    def update_by_id(self, id, conversation):
+        data = self.fetch_id(id)
+        if data:
+            data['data'].append(conversation)
+            updated_data_str = json.dumps(data)
+            self.cursor.execute(f'UPDATE {self.table} SET data = ? WHERE id = ?', (updated_data_str, id))
+        else:
+            updated_data_str = json.dumps({'data' : [conversation]})
+            self.cursor.execute(f'INSERT INTO {self.table} (id, data) VALUES (?, ?)', (id, updated_data_str))
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.close()
